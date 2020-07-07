1c914ff61aaac768325d5b60e0c070d754353c6c
Walter Fettich <wfettich@gmail.com>
2020-07-07 12:56:45 +0300

    no message

--- KeyboardDemo/SceneDelegate.swift
+++ KeyboardDemo/SceneDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@available(iOS 13, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -47,7 +48,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
